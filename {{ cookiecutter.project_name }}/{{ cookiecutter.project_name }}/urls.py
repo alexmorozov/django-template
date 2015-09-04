8e9d0d8acf371b66a6c1a8bae16d918a045a1bdc
Alex Morozov <morozov@podmoskovie.info>
2015-09-04 12:50:29 +0300

    Исправил опечатку

--- {{ cookiecutter.project_name }}/{{ cookiecutter.project_name }}/urls.py
+++ {{ cookiecutter.project_name }}/{{ cookiecutter.project_name }}/urls.py
@@ -10,7 +10,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     '',
 
-    url(r'^$', TemplateView.as_view(TemplateView='index.html'), name='index'),
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
     url(r'^robots.txt$', TemplateView.as_view(template_name='robots.txt',
                                               content_type='text/plain')),
     url(r'^admin/', include(admin.site.urls)),
