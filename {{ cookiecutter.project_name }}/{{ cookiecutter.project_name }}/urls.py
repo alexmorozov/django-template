46f86303556cfd5d0249fd2be635ce6d54a5a75b
Alex Morozov <morozov@podmoskovie.info>
2015-09-04 12:51:47 +0300

    Убрал остаток от patterns().

--- {{ cookiecutter.project_name }}/{{ cookiecutter.project_name }}/urls.py
+++ {{ cookiecutter.project_name }}/{{ cookiecutter.project_name }}/urls.py
@@ -8,8 +8,6 @@ from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    '',
-
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
     url(r'^robots.txt$', TemplateView.as_view(template_name='robots.txt',
                                               content_type='text/plain')),
